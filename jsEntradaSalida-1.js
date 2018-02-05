5eb92c9650adfe34bbe82195693a7dbc959d8e77
Dionel1990 <36174118+Dionel1990@users.noreply.github.com>
2018-02-05 20:58:43 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,24 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+
+{
+//esto es un comentario
+//alert(anda)
+//toda linea de instruccion termina en punto y coma (;)
+/* esto es un comentario en bloque
+var nombre;
+alert("nombre");
+alert(nombre);
+nombre="dio";
+alert(nombre);
+var person=prompt("please enter your name","harry potter");
+/*
+nombre=prompt("ingrese su nombre","dio");
+//siempre separamos los terminos por comas
+/*
+alert("su nombre es:");
+alert(nombre);
+*/
+alert("su nombre es:"+nombre;)
+}
+
