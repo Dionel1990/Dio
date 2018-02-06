77d52887658a8a49b91c063db037940fb7bc1d4c
Dionel1990 <36174118+Dionel1990@users.noreply.github.com>
2018-02-05 21:59:59 -0300

    Add files via upload
    aprendiendo

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,12 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{
+	var nombre;
+	nombre=prompt("por favor imgresar tu nombre");
+	documnet.getElementById('elNombre').value=nombre;
+
+}
+
